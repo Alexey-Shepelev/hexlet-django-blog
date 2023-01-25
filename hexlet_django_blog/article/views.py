10646f88177baf9e1c1a294cd13444ee60321997
Alexey-Shepelev <alexey.sailor@gmail.com>
2023-01-25 18:07:13 +0300

    Lesson 5

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
-def index(request):
-    return render(request, 'article/index.html', context={
-        'application': 'Article',
-    })
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'article/index.html', context={
+                'application': 'Article',
+            })
+
+
+# def index(request):
+#     return HttpResponse('Hello, World!')
